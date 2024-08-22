40d9940306d42fa34f448b8e9fbbeb9b8c67dfd9
Claire Chen <81056267+claireluchen@users.noreply.github.com>
2024-08-22 03:38:55 -0700

    Update jacobianTest.py

--- jacobianTest.py
+++ jacobianTest.py
@@ -11,25 +11,14 @@ input = torch.tensor([[2., 3., 4.]], requires_grad=True)
 
 #JACOBIAN TAKES FUNCTION THAT PRODUCES 3 OUTPUTS W.R.T. 3 INPUTS
 output = torch.autograd.functional.jacobian(func, input)
-print(output.shape)
-print(output)
+# print(output.shape)
+# print(output)
 
 output_squeezed = output.sum(dim=2)
+print(output_squeezed.shape) #[1, 3, 3]
 print(output_squeezed)
-
-jacobian = torch.stack([output_squeezed, output_squeezed, output_squeezed])
-# print(jacobian)
-
-#generating X
-x_tmp = torch.arange(1, 10)
-x = x_tmp.view(3, 3)
-# print("x", x)
-
-#multiplication
-x_expanded = x.unsqueeze(1) #[3, 1, 3]
-# print("x_expanded:", x_expanded)
-# print(jacobian * x_expanded)
-
-result = torch.sum(jacobian * x_expanded, dim=2)
-# print("result", result)
-# print(result.shape)
+'''
+[[[ 48.,   2.,   1.],
+[  4., 108.,   2.],
+[  2.,   6.,   1.]]]
+'''
